314eb492593a1db5e174de16f0fd8cc2544dfd21
Jeremy Pratt <jerpratt1@gmail.com>
2023-07-27 14:09:44 -0400

    set up

---
+++ Java/src/main/java/com/kiosk_v1/kiosk_v1/KioskV1Application.java
@@ -0,0 +1,13 @@
+package com.kiosk_v1.kiosk_v1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KioskV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KioskV1Application.class, args);
+	}
+
+}
---
+++ Java/src/test/java/com/kiosk_v1/kiosk_v1/KioskV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kiosk_v1.kiosk_v1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KioskV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
